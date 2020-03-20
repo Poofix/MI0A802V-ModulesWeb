87eba4b7e59342aff6e7926d4e5288b0a1936fd3
Poofix <ancelin.glories@gmail.com>
2020-03-20 15:15:19 +0100

    Add read simple file

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 
 *PDF :*
 - [Partie 1](./ressources/NEM_partie1.pdf)
+***
+## Partie 1
+
+* Lire un fichier [ici](./part1/readFile/README.md)
+
---
+++ part1/readFile/script.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+
+if (process.argv[2]) {
+    fs.readFile('./data.csv', 'utf-8', (err, data) =>{
+        if(err){
+            console.error(err);
+            return;
+        }
+        console.log(data);
+    })
+} else {
+    console.error("Veuillez précisez le nom du fichier à lire");
+}
